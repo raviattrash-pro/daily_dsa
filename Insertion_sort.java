786f3c5b037ef83f6b4d67f46a87cb38b5ba9ce2
raviattrash-pro <raviattrash1@gmail.com>
2025-07-17 17:04:11 +0530

    sorting

--- Insertion_sort.java
+++ Insertion_sort.java
@@ -33,3 +33,45 @@ class Insertion_sort{
 		return arr;
 	}
 }
+
+
+/*
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*class Solution {
+    public ListNode insertionSortList(ListNode head) {
+          if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0); // Dummy node for the new sorted list
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode prev = dummy;
+            // Find the right place to insert curr
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+
+            ListNode nextTemp = curr.next; // Save next node
+            // Insert curr between prev and prev.next
+            curr.next = prev.next;
+            prev.next = curr;
+
+            curr = nextTemp; // Move to the next node
+        }
+
+        return dummy.next;
+    }
+}
+
+
+*/
